a38df526a22fb5122b936ff670b7c9d9b8c6f9c0
David De Anda <omarandstuff@gmail.com>
2022-12-25 22:10:59 -0600

    Right return type for interface

--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,6 @@ export interface EngineInterface {
   set: (token: string, subject: Record<string, any>, category?: string) => void | Promise<void>
   get: (token: string) => Record<string, any> | Promise<Record<string, any>>
   delete: (token: string) => void | Promise<void>
-  getCategory: (category: string) => TokenRegistry | Promise<TokenRegistry[]>
+  getCategory: (category: string) => Record<string, any> | Promise<Record<string, any>[]>
   listCategories: () => string[] | Promise<string[]>
 }
